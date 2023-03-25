8b126af49effa6d552ff6f752045d8b83bd83e98
lesley-byte <phosphorescence3@hotmail.com>
2023-03-25 18:13:57 -0400

    added font to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,19 +2,33 @@ import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Iceland', sans-serif",
+  },
+});
 
 const Header = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Checklistle
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+    <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Iceland&display=swap');
+      </style>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, fontSize: "36px" }}
+            >
+              Checklistle
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </>
   );
 };
 
